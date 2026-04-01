2137e693c98cdbadf8279c80d6eb7e7a22eae3b4
Goootaa <guilhermeduarte656@gmail.com>
2026-04-01 12:09:44 -0400

    Modified

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,29 +1,83 @@
-using Unity.Mathematics;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
 
-public class Movement : MonoBehaviour
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
 {
-    public float sensibility = 200f;
-    public Transform player;
-    float xrotation = 0f;
-    private CharacterController controller;
-    public float speed = 10.0f;
-    private float gravity = -9.81f;
-    public Rigidbody rb;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Camera playerCamera;
+    public float walkSpeed = 6f;
+    public float runSpeed = 12f;
+    public float jumpPower = 7f;
+    public float gravity = 10f;
+    public float lookSpeed = 2f;
+    public float lookUpLimit = 45f;
+    public float LookDownLimit = 45f;
+    public float defaultHeight = 2f;
+    public float crouchHeight = 1f;
+    public float crouchSpeed = 3f;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private float rotationX = 0;
+    private CharacterController characterController;
+
+    private bool canMove = true;
+
     void Start()
     {
-         controller = GetComponent<CharacterController>();
-        //Aplica uma força ao objeto
-        //rb.GetComponent<Rigidbody>().AddForce(new Vector3(3,0,0));
+        characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float inputx = Input.GetAxisRaw("Horizontal");
-        float inputz = Input.GetAxisRaw("Vertical");
-        Vector3 movement = new Vector3(inputx, -9.81f, inputz);
-        controller.Move(movement * Time.deltaTime * speed); 
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+        if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
+        {
+            moveDirection.y = jumpPower;
+        }
+        else
+        {
+            moveDirection.y = movementDirectionY;
+        }
+
+        if (!characterController.isGrounded)
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.C) && canMove)
+        {
+            characterController.height = crouchHeight;
+            walkSpeed = crouchSpeed;
+            runSpeed = crouchSpeed;
+
+        }
+        else
+        {
+            characterController.height = defaultHeight;
+            walkSpeed = 6f;
+            runSpeed = 12f;
+        }
+
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        if (canMove)
+        {
+            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotationX = Mathf.Clamp(rotationX, -LookDownLimit, lookUpLimit);
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        }
     }
 }
