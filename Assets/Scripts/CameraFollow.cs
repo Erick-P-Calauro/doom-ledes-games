305a521852e55db9f49a78f64ec02e09d78c2274
Goootaa <guilhermeduarte656@gmail.com>
2026-04-01 23:42:25 -0400

    Modified: CameraFollow just rotate in x axis now

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,19 +1,19 @@
 using UnityEngine;
 
+
+//Atenção, para o script funcionar a camera de visão do player deve ter a tag MainCamera
 public class CameraFollow : MonoBehaviour
 {
     private Camera cam;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         cam = Camera.main;
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         Debug.Log("billboard rodando");
-        transform.rotation = cam.transform.rotation;
+        transform.rotation = Quaternion.Euler(0, cam.transform.eulerAngles.y, 0);
     }
 }
