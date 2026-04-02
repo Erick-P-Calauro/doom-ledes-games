a6d9e88c7f6f06a89033d59bf4e7f462a37af22d
Goootaa <guilhermeduarte656@gmail.com>
2026-04-01 22:58:06 -0400

    Add: enemy follows player using unity AI

---
+++ Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+/*Este código foi feito com a pré suposição de que o unity do usuário possua o AI navegator.
+Caso o usuário não tenha o pacakge (ou queira conferir), siga as seguintes instruções:
+dentro do editor, Window-> Package Management-> package manager-> Unity Registry. Search: "AI Navigation"
+Este script é inserido no inimigo(enemy) e 
+*/
+public class EnemyFollow : MonoBehaviour
+{
+    private Transform player;
+    private NavMeshAgent agent;
+    public float detectionRadius = 10f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position,player.position);
+        if(distance <= detectionRadius)
+        {
+            agent.SetDestination(player.position);   
+        }
+        else
+        {
+            agent.ResetPath();
+        }        
+    }
+}
